9a4c4c5d46559d70bc8fbbb77c798cbba54153d9
alreem <alreemmaskari@gmail.com>
2024-07-10 12:42:36 +0400

    commit main

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// yes, the main is here before rebase 
+// yes, the main is here before rebase, alreem
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
