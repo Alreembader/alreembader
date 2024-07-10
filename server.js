d95715912021b413a9ece83197705f3bb5c74220
alreem <alreemmaskari@gmail.com>
2024-07-10 12:26:49 +0400

    saving changes before rebase

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// yes the baranch2 is here
+// yes, the main is here before rebase 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
